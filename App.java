9d9d7e082886eebad40164f62c24f5a64347f4ac
xxxx <sginc982@gmail.com>
2023-06-04 12:08:58 -0600

    first commit

---
+++ App.java
@@ -0,0 +1,10 @@
+package week06FINALPROJECT;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ card.java
@@ -0,0 +1,5 @@
+package week06FINALPROJECT;
+
+public class card {
+
+}
