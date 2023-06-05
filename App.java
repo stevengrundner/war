7e7921759c3bab8630dff14024f23a2a0ab0f976
xxxx <sginc982@gmail.com>
2023-06-05 15:39:53 -0600

    stopped at 1c

--- App.java
+++ App.java
@@ -3,8 +3,25 @@ package week06FINALPROJECT;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		
+	Deck deck = new Deck();
+	deck.describe();
+	
+	System.out.println("**********************************************************************");
+	
+	deck.shuffle();
+	deck.describe();
+
+	System.out.println("**********************************************************************");
+
+	Card drawnCard = deck.draw();
+	drawnCard.describe();
+		
+	System.out.println("**********************************************************************");
+
+	System.out.println();
+	
+		}
 
 	}
-
-}
--- player.java
+++ player.java
@@ -1,5 +1,7 @@
 package week06FINALPROJECT;
 
-public class player {
+public class Player {
+	
+
 
 }
